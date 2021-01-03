d4d51ff56096d7457c90aed546afd089f6d6dcd5
Reyes Montemayor <sjpete1234@hotmail.com>
2021-01-03 18:40:53 -0500

    How to comment out as a block

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -10,9 +10,9 @@ class BasicsDemo {
     System.out.println("\n\nInside print ...");
     System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
     System.out.println();                 // Print empty line
-    System.out.print("Hello, world!!");   // Cursor stayed after the printed string
+    /* System.out.print("Hello, world!!");   // Cursor stayed after the printed string
     System.out.println("Hello,");
-    System.out.print(" ");                // Print a space
+    System.out.print(" ");  */              // Print a space
     // System.out.print("world!!");
   }
 
