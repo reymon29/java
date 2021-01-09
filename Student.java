57d6294a0abf15310d920a154f4bb10afe1f18e0
Reyes Montemayor <sjpete1234@hotmail.com>
2021-01-08 22:29:21 -0500

    Learning float

--- Student.java
+++ Student.java
@@ -1,22 +1,19 @@
 class Student {
   int id = 1000;
   byte age = 1_8;
-  short rank = 165; //[-128, 127]
-  long phone = 223_456_7890L; // Java -- readability
 
-  // Integer literals: int literal, long literal
+  long phone = 223_456_7890L;
+  float gpa = 3.8f;
 
-  int minValue = Integer.MIN_VALUE;
-  int maxValue = Integer.MAX_VALUE;
+
 
   void compute() {
     int nextId = id + 1;
     System.out.println("id: " + id);
     System.out.println("nextId: " + nextId);
     System.out.println("age: " + age);
-    System.out.println("phone: " + phone);
-    System.out.println("minValue: " + minValue);
-    System.out.println("maxValue: " + maxValue);
+    System.out.println("gpa: " + gpa);
+
   }
 
   public static void main(String[] args){
