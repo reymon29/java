b851ab6a6ce409577f01f185430041d189dfdd86
Reyes Montemayor <sjpete1234@hotmail.com>
2021-01-11 21:56:57 -0500

    Learning booleans

--- Student.java
+++ Student.java
@@ -1,19 +1,25 @@
 class Student {
   int id = 1000;
-  byte age = 1_8;
-
+  byte age = 18;
   long phone = 223_456_7890L;
   double gpa = 3.8;
-
   char degree = 'B';
+  boolean international;
+  double tuitionFees = 12000.0;
+  double internationFees = 5000.0;
 
   void compute() {
     int nextId = id + 1;
+    if (international) {
+      tuitionFees = tuitionFees + internationFees;
+    }
+
     System.out.println("id: " + id);
     System.out.println("nextId: " + nextId);
     System.out.println("age: " + age);
     System.out.println("gpa: " + gpa);
     System.out.println("degree: " + degree);
+    System.out.println("tuitionFees: " + tuitionFees);
 
   }
 
