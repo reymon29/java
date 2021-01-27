b8a8289c5157fd672dc0539d8d13322df6540f53
Reyes Montemayor <sjpete1234@hotmail.com>
2021-01-26 22:19:43 -0500

    Class assignment to update current converter using arrays

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,24 +1,15 @@
 class CurrencyConverter {
-  int rupee = 63;
-  int dirham = 3; //UAE
-  int real = 3; //brazilian
-  int chilean_peso = 595;
-  int mexican_peso = 18;
-  int _yen = 107;
-  int $austrailian = 2; //austrailian dollar
-  int dollar;
-  int Rupee63;
 
   void printCurrencies(){
-    System.out.println("rupee " + rupee);
-    System.out.println("dirham " + dirham);
-    System.out.println("real " + real);
-    System.out.println("chilean_peso " + chilean_peso);
-    System.out.println("mexican_peso " + mexican_peso);
-    System.out.println("_yen " + _yen);
-    System.out.println("$australian: " + $austrailian);
-    System.out.println("dollar " + dollar);
-    System.out.println("Rupee63 " + Rupee63);
+    double[] exchangeRates = {63,3,3,595,18,107,2};
+
+    System.out.println("rupee " + exchangeRates[0]);
+    System.out.println("dirham " + exchangeRates[1]);
+    System.out.println("real " + exchangeRates[2]);
+    System.out.println("chilean_peso " + exchangeRates[3]);
+    System.out.println("mexican_peso " + exchangeRates[4]);
+    System.out.println("_yen " + exchangeRates[5]);
+    System.out.println("$australian: " + exchangeRates[exchangeRates.length - 1]);
   }
 
   public static void main(String[] args){
