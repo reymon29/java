a214c0e2dd336cfaa89e692d3045acfe0d3fae98
Reyes Montemayor <sjpete1234@hotmail.com>
2021-01-26 21:14:55 -0500

    Learning objects and null pointer exception

--- Student.java
+++ Student.java
@@ -30,6 +30,8 @@ class Student {
 
   }
 
+  static Student student3;
+
   public static void main(String[] args){
     Student student1 = new Student();
     student1.id = 1000;
@@ -53,7 +55,7 @@ class Student {
     student2.international = true;
     student2.compute();
 
-    Student student3 = new Student();
+    // Student student3 = new Student();
     student3.id = 1002;
     student3.name = "Anita";
     student3.gender = "female";
