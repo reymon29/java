cc1694e622af5222586aba072394e050babef06d
Reyes Montemayor <sjpete1234@hotmail.com>
2021-01-30 09:52:14 -0500

    learning instance methods

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,9 +1,8 @@
 class CurrencyConverter {
+  double[] exchangeRates;
 
   void printCurrencies(){
-    double[] exchangeRates = {63,3,3,595,18,107,2};
-
-    System.out.println("rupee " + exchangeRates[0]);
+    System.out.println("\nrupee " + exchangeRates[0]);
     System.out.println("dirham " + exchangeRates[1]);
     System.out.println("real " + exchangeRates[2]);
     System.out.println("chilean_peso " + exchangeRates[3]);
@@ -12,8 +11,37 @@ class CurrencyConverter {
     System.out.println("$australian: " + exchangeRates[exchangeRates.length - 1]);
   }
 
+  void setExchangeRates(double [] rates) {
+    exchangeRates = rates;
+  }
+
+  void updateExchangeRate(int arrayIndex, double newVal) {
+    exchangeRates[arrayIndex] = newVal;
+  }
+
+  double getExchangeRate(int arrayIndex){
+    return exchangeRates[arrayIndex];
+  }
+
+  double computeTransferAmount(int arrayIndex, double amount){
+    return amount * getExchangeRate(arrayIndex);
+  }
+
   public static void main(String[] args){
     CurrencyConverter cc = new CurrencyConverter();
+    // Jan 1
+    double[] rates = {63,3,3,595,18,107,2};
+    cc.setExchangeRates(rates);
     cc.printCurrencies();
+    // Adding a new array after already declared
+    rates = new double[]{65,5,3,595,18,107,2};
+    cc.setExchangeRates(rates);
+    cc.printCurrencies();
+    // Invoking the new method updateExchangeRate
+    cc.updateExchangeRate(0, 66);
+    cc.printCurrencies();
+    // Invoking the new method computeTransferAmount / getExchangeRate
+    double amount = cc.computeTransferAmount(0, 1000);
+    System.out.println("\nTransferred amount USD > rupee: " + amount);
   }
 }
