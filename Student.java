4748a2f1b54b89bbb2795a645e5105edffe4be5c
Rey <36433822+reymon29@users.noreply.github.com>
2021-02-19 16:11:01 -0500

    Reinitialize objects

--- Student.java
+++ Student.java
@@ -32,7 +32,7 @@ class Student {
         tuitionFees = tuitionFees + internationFees;
       }
 
-      System.out.println("\nid: " + this.id);
+      /* System.out.println("\nid: " + this.id);
       System.out.println("nextId: " + nextId);
       System.out.println("name: " + this.name);
       System.out.println("gender: " + this.gender);
@@ -41,7 +41,7 @@ class Student {
       System.out.println("gpa: " + this.gpa);
       System.out.println("degree: " + this.degree);
       System.out.println("tuitionFees: " + this.tuitionFees);
-      System.out.println("studentCount: " + studentCount);
+      System.out.println("studentCount: " + studentCount); */
     }
 
   Student() {}
@@ -53,17 +53,4 @@ class Student {
 
   static Student student3;
 
-  public static void main(String[] args){
-    Student student1 = new Student(1000, "Joan", "male", 18, 223_456_7890L, 3.8, 'b');
-    Student student2 = new Student(1001, "Raj", "male", 21, 223_456_9990L, 3.4, 'M', true);
-    Student student3 = new Student(1002, "Anita", "female", 20, 223_456_8888L, 4.0, 'M', true);
-    System.out.println("\nStudent.studentCount: " + Student.studentCount);
-
-    System.out.println("\nName of student1: " + student1.name);
-    System.out.println("Name of student2: " + student2.name);
-    System.out.println("Name of student3: " + student3.name);
-
-    student1.updateProfile("John");
-    System.out.println("\nUpdated name of student1: " + student1.name);
-  }
 }
