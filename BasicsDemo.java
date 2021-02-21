16e51e6541df1f339490459086fe990a7dabcf94
Rey <36433822+reymon29@users.noreply.github.com>
2021-02-21 13:57:24 -0500

    bitwiseOperators

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -281,6 +281,27 @@ class BasicsDemo {
 		Student s1 = new Student(1001, "Raj");
 		update(s1, "Dheeru");
 	}
+
+  static void bitwiseOperators() {
+    System.out.println("\nInside bitwiseOperators ...");
+    int x = 1;
+  int y = 3;
+  
+  System.out.println("x & y: " + (x & y));
+  System.out.println("x | y: " + (x | y));
+  System.out.println("x ^ y: " + (x ^ y));
+  System.out.println("~x: " + (~x));
+  System.out.println("true & false: " + (true & false));
+   
+  char c1 = 'a'; // U+0061 --> 0110 0001
+  char c2 = 'b'; // U+0062 --> 0110 0010
+  System.out.println("c1 | c2: " + (c1 | c2)); // 0110 0011 --> 99 in decimal		
+  
+  // Since bitwise work only on Integer types, following will not compile
+  //double d1 = 3.14;
+  //double d2 = 5.15;
+  //System.out.println("d1 | d2: " + (d1 | d2));
+  }
 	
 
   public static void main(String[] args){
@@ -310,6 +331,7 @@ class BasicsDemo {
     // isOddOrEven(50);
     // charTypePromotion();
     // comparisonOperatios();
-    logicalOperators();
+    // logicalOperators();
+    bitwiseOperators();
   }
 }
