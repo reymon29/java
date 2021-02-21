54c8830aa54e1aaa6582b230da92dd477caefa96
Rey <36433822+reymon29@users.noreply.github.com>
2021-02-21 08:18:31 -0500

    Logical Operators

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -239,6 +239,45 @@ class BasicsDemo {
     return true;
   }
 
+  static void logicalOperators() {
+		System.out.println("\nInside logicalOperators ...");
+		int age = 37;
+	    int salary = 85000;
+	    boolean hasBadCredit = false;
+		
+		// 1. Core (AND, OR, NOT & Operator Chaining)
+	    
+	    if (age > 35 && salary > 90000 || !hasBadCredit) {
+	    	System.out.println("Loan approved!");
+	    } else {
+			System.out.println("Loan not approved!");
+		}
+		
+		// 2. 
+		//    (a) Left-associative ~ Order of grouping
+		//    (b) Associativity (a && b) && c = a && (b && c)
+		//    Applies to both && and ||
+		
+		
+		// 3. 
+		//    (a) Operator precedence of Logical Operators: Helps with ONLY grouping operations. Not order of execution. (! > && > ||)
+		
+		          // Other Examples: A && B || C && D = (A && B) || (C && D)
+				  //                 A && B && C || D = ((A && B) && C) || D
+		
+		
+		//    (b) Operator Precedence across logical, comparison and arithmetic
+		//          ! > arithmetic > comparison > &&, ||
+		//    See resources section for complete precedence rules
+		
+		// ALWAYS USE PARENTHESIS for READABILITY. Not everyone is aware of precedence rules
+		
+		// 4. Use && to avoid NullPointerException
+		Student s1 = new Student(1001, "Raj");
+		update(s1, "Dheeru");
+	}
+	
+
   public static void main(String[] args){
     // print();
     // primitives();
@@ -265,6 +304,7 @@ class BasicsDemo {
     // compoundArithmeticAssigment();
     // isOddOrEven(50);
     // charTypePromotion();
-    comparisonOperatios();
+    // comparisonOperatios();
+    logicalOperators();
   }
 }
