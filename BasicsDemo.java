89c308bcfac8081fb91a381793504603ead28954
Rey <36433822+reymon29@users.noreply.github.com>
2021-02-20 19:50:23 -0500

    Arithmetic Operation rules

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -196,6 +196,16 @@ class BasicsDemo {
   static void isOddOrEven(int num){
     System.out.println(num % 2);
   }
+
+  static void charTypePromotion() {
+	  System.out.println("\nInside charTypePromotion ...");
+	  char char1 = 50; // Will be assigned corresponding UTF16 value 2
+	  System.out.println("char1: " + char1); 
+	  System.out.println("(73 - char1): " + (73 - char1)); // char1 gets promoted to int, i.e., decimal equivalent 50 in UTF16 is used	  
+	  System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints	
+	  System.out.println("('a' + 'b'): " + ('a' +'b')); // 'a' & 'b' are promoted to ints and the respective equivalents 97 & 98 are added
+  }
+
   public static void main(String[] args){
     // print();
     // primitives();
@@ -220,6 +230,7 @@ class BasicsDemo {
     // varargsOverLoad(true);
     // preAndPost();
     // compoundArithmeticAssigment();
-    isOddOrEven(50);
+    // isOddOrEven(50);
+    charTypePromotion();
   }
 }
