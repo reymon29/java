ba98cea867c22df4530a69e527231478ed2d1368
Rey <36433822+reymon29@users.noreply.github.com>
2021-02-25 16:03:08 -0500

    packages

--- com/semticsquare/basics/StudentTest.java
+++ com/semticsquare/basics/StudentTest.java
@@ -1,3 +1,5 @@
+package com.semticsquare.basics;
+
 public class StudentTest {
 
     public static void main(String[] args){
@@ -12,38 +14,5 @@ public class StudentTest {
 
         student1.updateProfile("John");
         System.out.println("\nUpdated name of student1: " + student1.name);
-
-       /* Student student4 = student1;
-        System.out.println("\nName of student4: " + student4.name);
-
-        student4.updateProfile("Mike");
-        System.out.println("Name of student1: " + student1.name);
-
-        student4 = student2;
-        System.out.println("Name of student4 " + student4.name);
-
-        student2 = student1;
-        System.out.println("Name of student4: " + student4.name);
-        System.out.println("Name of student2: " + student2.name);
-
-        student4 = new Student();
-        student4.updateProfile("Alex");
-        System.out.println("Name of student4: " + student4.name);
-        System.out.println("Name of student2: " + student2.name);
-        System.out.println("Name of student1: " + student1.name); */
-
-        Student[] students = {student1, student2, student3};
-
-        swap(students, 0, 2);
-
-        System.out.println("Name of student4: " + students[0].name);
-        System.out.println("Name of student2: " + students[1].name);
-        System.out.println("Name of student1: " + students[2].name);
-    }
-    
-    static void swap(Student[] students, int firstIndex, int secondIndex) {
-        Student temp = students[firstIndex];
-        students[firstIndex] = students[secondIndex];
-        students[secondIndex] = temp;
     }
 }
