0d19e87d515bec75130478f9f45bc57990a6d022
Rey <36433822+reymon29@users.noreply.github.com>
2021-02-28 09:41:50 -0500

    Enhancing Student Example with Information Hiding

--- com/semticsquare/basics/Student.java
+++ com/semticsquare/basics/Student.java
@@ -1,22 +1,92 @@
-class Student {
-  // variable declarations
-  int id; 
-  String name; 
-  String gender = "male";
+package com.semanticsquare.basics;
 
-  // Constructors
-  Student(int id, String name) {
-      this(name); // Invoking overloaded constructor. If present, should be first statement
-      this.id = id;
-  }
+public class Student {
+	static int studentCount;
 
-  Student(String name) {
-      this.name = name;
-  }
+	private int id;
+	private String name;
+	private int iGender;
+	private String gender;
+	private int age;
+	private long phone;
+	private double gpa;
+	private char degree;
 
-  // method definitions
-  boolean updateProfile(String name) {
-      this.name = name;
-      return true;
-  }
+	private boolean international;
+	private double tuitionFees = 12000.0;
+	private double internationalFees = 5000.0;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		if (gender.equals("male")) {
+			iGender = 1;
+		} else if (gender.equals("female")) {
+			iGender = 2;
+		} else if (gender.equals("transgender")) {
+			iGender = 3;
+		}
+		if (iGender == 0) {
+			throw new IllegalArgumentException("Invalid gender!!");			
+		} else {
+			this.gender = gender;
+		}
+		
+	}
+
+	public Student(int newId, String newName, String newGender, int newAge, long newPhone, double newGpa,
+			char newDegree) {
+		this(newId, newName, newGender, newAge, newPhone, newGpa, newDegree, false);
+	}
+
+	public Student(int id, String name, String gender, int age, long phone, double gpa, char degree,
+			boolean international) {
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.phone = phone;
+		this.gpa = gpa;
+		this.degree = degree;
+		this.international = international;
+
+		studentCount = studentCount + 1;
+		int nextId = id + 1;
+
+		if (international) {
+			tuitionFees = tuitionFees + internationalFees;
+			// return;
+		}
+
+		System.out.println("\nid: " + this.id);
+		System.out.println("nextId: " + nextId);
+		System.out.println("name: " + this.name);
+		System.out.println("gender: " + this.gender);
+		System.out.println("age: " + this.age);
+		System.out.println("phone: " + this.phone);
+		System.out.println("gpa: " + this.gpa);
+		System.out.println("degree: " + this.degree);
+		System.out.println("tuitionFees: " + tuitionFees);
+		System.out.println("studentCount: " + studentCount);
+
+	}
+
+	public Student() {
+	}
+
+	public boolean updateProfile(String name) {
+		this.name = name;
+		return true;
+	}
+
 }
